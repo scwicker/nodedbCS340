016c9489b1033a7ae7c0df07b4cf682c016b298c
scwicker <31528158+scwicker@users.noreply.github.com>
2018-12-07 15:37:35 -0500

    Update dbcon.js

--- views/dbcon.js
+++ views/dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_wickerss',
-  password        : '2621',
+  password        : '',
   database        : 'cs340_wickerss'
 });
 
